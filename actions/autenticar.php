6aa9ec2469f38fa2aef01c9bcdfe3a0a6a0f5e0c
brenoach <brenoach@gmail.com>
2026-03-26 21:49:35 -0300

    bd

--- actions/autenticar.php
+++ actions/autenticar.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../connection/config.php';
+require_once __DIR__ . '/../includes/db.php';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
