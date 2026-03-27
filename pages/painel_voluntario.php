89aa9339c7aadf2a8f686dc23dddd07c14ca4f24
brenoach <brenoach@gmail.com>
2026-03-27 01:27:09 -0300

    $

--- pages/painel_voluntario.php
+++ pages/painel_voluntario.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../connection/config.php';
 require_once __DIR__ . '/../includes/auth.php';
-verificarAcesso('voluntario'); 
+// verificarAcesso('voluntario'); 
 
 require_once __DIR__ . '/../includes/db.php';
 require_once __DIR__ . '/../includes/helpers.php'; // Aqui está a sua função exibirFoto!
